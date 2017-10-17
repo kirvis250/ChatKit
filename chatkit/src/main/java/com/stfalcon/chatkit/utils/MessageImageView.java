35b8118836eabf3387791814c1a5c5c08e154223
Mindaugas Viburys <kirvis250@gmail.com>
2017-10-17 17:15:22 +0300

    minor chaanges

---
+++ chatkit/src/main/java/com/stfalcon/chatkit/utils/MessageImageView.java
@@ -0,0 +1,28 @@
+package com.stfalcon.chatkit.utils;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by kirvis250 on 17.10.17.
+ */
+
+public class MessageImageView extends AppCompatImageView {
+
+    public MessageImageView(Context context) {
+        super(context);
+    }
+
+    public MessageImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MessageImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
