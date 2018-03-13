493be3ee49ff355518d1a54e4e68a4f52d89cd3d
Mindaugas Viburys <kirvis250@gmail.com>
2018-03-13 15:30:51 +0200

    shit

--- chatkit/src/main/java/com/stfalcon/chatkit/utils/MessageImageView.java
+++ chatkit/src/main/java/com/stfalcon/chatkit/utils/MessageImageView.java
@@ -1,6 +1,9 @@
 package com.stfalcon.chatkit.utils;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
@@ -24,5 +27,20 @@ public class MessageImageView extends AppCompatImageView {
         super(context, attrs, defStyleAttr);
     }
 
+    Path clipPath = new Path();
+    RectF rect = null;
+    float radius = 36.0f;
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (rect == null) {
+            rect = new RectF(0, 0, this.getWidth(), this.getHeight());
+        }
+
+        clipPath.addRoundRect(rect, radius, radius, Path.Direction.CW);
+        canvas.clipPath(clipPath);
+        super.onDraw(canvas);
+    }
 
 }
